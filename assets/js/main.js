843614c1c23fe285c322790a15dd207b994141ca
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2023-12-23 22:54:30 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@ const weather_temparature = document.querySelector('.weather-temparature');
 const weather_type = document.querySelector('.weather-type');
 const weather_info = document.querySelector('.weather-info');
 
+const city_name = document.querySelector('.name');
+
 const my_api_keys = '1b8d7bdc1f47771d5acd90e65786747d';
 
 const weather_conditions = ['Clouds', 'Clear', 'Rain', 'Snow', 'Thunderstorm', 'Drizzle', 'Mist', 'Haze', 'Fog', 'Smoke', 'Dust', 'Sand', 'Ash', 'Squall', 'Tornado'];
@@ -18,22 +20,25 @@ search_btn.addEventListener('click', (e) => {
     not_found.classList.remove('fade-in');
     weather_info.classList.remove('fade-in');
     weather_details.classList.remove('fade-in');
+    city_name.classList.remove('fade-in');
     const city = input.value;
+    input.value = '';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${my_api_keys}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            // console.log(data)
             if(data.cod === '404') {
                 not_found.style.display = 'block';
                 not_found.classList.add('fade-in');
-                container.style.height = '350px'
+                container.style.height = '355px'
                 weather_details.style.display = 'none';
                 return;
             }
+
             not_found.style.display = 'none';
             not_found.classList.remove('fade-in');
 
-            
             weather_img.src = '';
             const current_weather = data.weather[0].main;
             for(let i = 0; i < weather_conditions.length; i++) {
@@ -45,6 +50,11 @@ search_btn.addEventListener('click', (e) => {
             weather_temparature.textContent = `${parseInt(data.main.temp)}°C`;
             weather_type.textContent = `${data.weather[0].description}`;
 
+            const current_city = data.name;
+            city_name.textContent = current_city;
+            city_name.classList.add('fade-in');
+            city_name.style.display = 'block';
+
             const weather_humidity = document.querySelector('.humidity');   
             const weather_wind = document.querySelector('.wind');
             console.log(weather_humidity, weather_wind);
@@ -52,7 +62,7 @@ search_btn.addEventListener('click', (e) => {
             weather_wind.textContent = `${data.wind.speed} Km/h`;
 
             // dislay 
-            container.style.height = '600px'
+            container.style.height = '700px'
             weather_info.style.display = 'block';
             weather_info.classList.add('fade-in');
             weather_details.style.display = 'flex'; 
